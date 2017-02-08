45e199d330be559db530027fd35b7bd0ea52526d
pray-for <822032714@qq.com>
2017-02-08 13:36:57 +0800

    Update JsoupService.java

--- app/src/main/java/com/example/zhangjiawen/education/util/JsoupService.java
+++ app/src/main/java/com/example/zhangjiawen/education/util/JsoupService.java
@@ -15,8 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by J。 on 2016/4/18.
- * 封装好的Jsoup工具类
+ * Created by zhangjiawen on 2017/1/23.
  */
 public class JsoupService {
     private static final String TAG = "JsoupService";
@@ -64,25 +63,6 @@ public class JsoupService {
     }
 
 
-//    /**
-//     * 获取修改密码的信息
-//     *
-//     * @param content 网页html源码
-//     * @return 返回修改密码后的状态信息
-//     */
-//    public static String getUpdatePasswordMessage(String content) {
-//        Document document = Jsoup.parse(content);
-//        Elements elements = document.select("script");
-//        try {
-//            Element element = elements.get(0);
-//            Log.d(TAG, element.data().split("'")[1]);
-//            return element.data().split("'")[1];
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-
     /**
      * 解析menu菜单以获得子菜单的url
      *
@@ -110,7 +90,7 @@ public class JsoupService {
         Document document = Jsoup.parse(content);
 //        Elements elements = document.select("table#Table6.blacktab tr");
         Elements elements = document.select("table#Table1.blacktab tr");
-        //移除无效的数据
+//        移除无效的数据
         elements.remove(0);
         elements.remove(0);
         elements.remove(1);
@@ -146,8 +126,8 @@ public class JsoupService {
         Document document = Jsoup.parse(content);
         Elements elements = document.select("input[name=__VIEWSTATE]");
         map.put("__VIEWSTATE", elements.get(0).val());
-        elements = document.select("input[name=__VIEWSTATEGENERATOR]");
-        map.put("__VIEWSTATEGENERATOR", elements.get(0).val());
+       /* elements = document.select("input[name=__VIEWSTATEGENERATOR]");
+        map.put("__VIEWSTATEGENERATOR", elements.get(0).val());*/
         elements = document.select("select[name=ddlXN] option");
         List<String> score_year = new ArrayList<>();
         for (Element element : elements) {
@@ -171,72 +151,55 @@ public class JsoupService {
      */
     public static ArrayList<CourseInfo> parseCourseScore(String content) {
         Document document = Jsoup.parse(content);
+
+//        Element element = document.getElementById("Datagrid1");
+//        ArrayList<CourseInfo> courseInfoArrayList = new ArrayList<>();
+//        if (element != null) {
+//            Elements elements = element.getElementsByTag("tr");
+//            elements.remove(0);
+//            for (int i = 1; i < elements.size(); i++){
+//                Elements elements1 = elements.get(i).getAllElements();
+//                CourseInfo courseInfo = new CourseInfo();
+//            }
+//        }
+
         Elements elements = document.select("table#Datagrid1.datelist tr");
         //移除无效项
         elements.remove(0);
         ArrayList<CourseInfo> courseInfoArrayList = new ArrayList<>();
+
         for (Element element : elements) {
             Elements elements1 = element.select("td");
             CourseInfo courseInfo = new CourseInfo();
-            /**
-             * 赋值学年
-             */
+            //赋值学年
             courseInfo.setYear(elements1.get(0).text());
-            /**
-             * 赋值学期
-             */
+            //赋值学期
             courseInfo.setSemester(elements1.get(1).text());
-            /**
-             * 赋值课程代码
-             */
+            //赋值课程代码
             courseInfo.setCourse_code(elements1.get(2).text());
-            /**
-             * 赋值课程名称
-             */
+            //赋值课程名称
             courseInfo.setCourse_name(elements1.get(3).text());
-            /**
-             * 赋值课程性质
-             */
+            //赋值课程性质
             courseInfo.setCourse_nature(elements1.get(4).text());
-            /**
-             * 赋值课程归属
-             */
+            //赋值课程归属
             courseInfo.setCourse_belong(elements1.get(5).text());
-            /**
-             * 赋值课程学分
-             */
+            //赋值课程学分
             courseInfo.setCourse_credit(elements1.get(6).text());
-            /**
-             * 赋值课程绩点
-             */
+            //赋值课程绩点
             courseInfo.setCourse_average_point(elements1.get(7).text());
-            /**
-             * 赋值课程分数
-             */
+            //赋值课程分数
             courseInfo.setCourse_score(elements1.get(8).text());
-            /**
-             * 赋值辅修标记
-             */
+            //赋值辅修标记
             courseInfo.setCourse_aid_remark(elements1.get(9).text());
-            /**
-             * 赋值补考成绩
-             */
+            //赋值补考成绩
             courseInfo.setCourse_make_up_score(elements1.get(10).text());
-            /**
-             * 赋值重修成绩
-             */
+            //赋值重修成绩
             courseInfo.setCourse_rebuild(elements1.get(11).text());
-            /**
-             * 赋值院系
-             */
+            //赋值院系
             courseInfo.setCourse_college(elements1.get(12).text());
-            /**
-             * 赋值备注
-             */
+            //赋值备注
             courseInfo.setRemark(elements1.get(13).text());
-            /**
-             * 赋值重修标记
-             */
+            //赋值重修标记
             courseInfo.setCourse_rebuild_mark(elements1.get(14).text());
 
             //添加到集合
@@ -255,13 +218,13 @@ public class JsoupService {
         Document document = Jsoup.parse(content);
         Map<String, String> map = new HashMap<>();
         Element number = document.getElementById("xh");//学号
-        Log.d("测试-------" , number+"");//null
+//        Log.d("测试-------" , number+"");
         Element name = document.getElementById("xm");//姓名
         Element sex = document.getElementById("lbl_xb");//性别
         Element college = document.getElementById("lbl_xy");//学院
         Element major = document.getElementById("lbl_zymc");//专业
         Element classes = document.getElementById("lbl_xzb");//班级
-        Element image = document.getElementById("xszp");//图片
+//        Element image = document.getElementById("xszp");//图片
         Log.d("个人信息测试" , number.text() + "---" + name.text() + "---" +college.text() +"---" + major.text());
         map.put("学号" , number.text());
         map.put("姓名" , name.text());
@@ -269,7 +232,7 @@ public class JsoupService {
         map.put("学院" , college.text());
         map.put("专业" , major.text());
         map.put("班级" , classes.text());
-        Log.d("学号" , number.text() + "---" + name.text() + "---" +college.text() +"---" + major.text());
+        Log.d("学号+姓名+院+专业" , number.text() + "---" + name.text() + "---" +college.text() +"---" + major.text());
 
         return map;
     }
